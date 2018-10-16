6df5535acadc9986736630ae2ff53ae572aacbb0
Tim Biden <timbiden@gmail.com>
2018-10-16 11:20:48 -0700

    Generic table data & layout.

--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -9,7 +9,39 @@ class ToDos extends Component {
     handleComplete = toDo => {};
     render() {
         return (
-            <h3>Items still to be done:</h3>
+            <div>
+                <h3>Items still to be done:</h3>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
