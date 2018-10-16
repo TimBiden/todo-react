f4011096747604b868988e60538c1d1e20c2c028
Tim Biden <timbiden@gmail.com>
2018-10-16 13:41:30 -0700

    Items displaying from fake API.

--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -8,41 +8,32 @@ class ToDos extends Component {
 
     handleComplete = toDo => {};
     render() {
-        return (
-            <div>
-                <h3>Items still to be done:</h3>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Order</th>
-                            <th scope="col">To Do</th>
-                            <th scope="col">Importance</th>
-                            <th scope="col">Urgency</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
+        return (<div>
+            <h3>Items still to be done:</h3>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Order</th>
+                        <th scope="col">To Do</th>
+                        <th scope="col">Importance</th>
+                        <th scope="col">Urgency</th>
+                        <th scope="col">Estimated Hours</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.toDos.map((todo, i) =>(
+                            <tr>
+                                <th scope="row">{i + 1}</th>
+                                <td>{todo.activity}</td>
+                                <td>{todo.importance}</td>
+                                <td>{todo.urgency}</td>
+                                <td>{todo.hours}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>);
     }
 }
 
