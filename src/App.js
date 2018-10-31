3829a3053e0b79ba26bb4562b53fd0b751c465bf
Tim Biden <timbiden@gmail.com>
2018-10-31 12:47:30 -0700

    Prettier.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
-import NavBar from './components/common/navbar';
-import ToDos from './components/toDoList';
-import NewToDo from './components/newToDo';
-import Home from './components/home';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/common/navbar";
+import ToDos from "./components/toDoList";
+import NewToDo from "./components/newToDo";
+import Home from "./components/home";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <main role="main" className="container">
-            <NavBar />
-            <h1>From DO to DONE</h1>
+      <main role="main" className="container">
+        <NavBar />
+        <h1>From DO to DONE</h1>
 
-            <div className='content'>
-                <Switch>
-                    <Route path='/newtodo' component={NewToDo} />
-                    <Route path='/todolist' component={ToDos} />
-                    <Route path='/' component={Home} />
-                </Switch>
-            </div>
-        </main>
+        <div className="content">
+          <Switch>
+            <Route path="/newtodo" component={NewToDo} />
+            <Route path="/todolist" component={ToDos} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div>
+      </main>
     );
   }
 }
