e7900fde5e8b74febbb48413c429ad49e35b5f04
Tim Biden <timbiden@gmail.com>
2018-10-31 13:21:28 -0700

    Redirect. Add notes.

--- src/App.js
+++ src/App.js
@@ -14,10 +14,14 @@ class App extends Component {
       <main role="main" className="container">
         <NavBar />
         <div className="content">
+          {/* These are activated in the order they're found. */}
+          {/* "/not-found" MUST go after everything else.*/}
           <Switch>
             <Route path="/newtodo" component={NewToDo} />
             <Route path="/todolist" component={ToDos} />
+            <Redirect from="/messages" to="/todolist" />
             <Route path="/" exact component={Home} />
+
             <Route path="/not-found" component={NotFound} />
             <Redirect to="/not-found" />
           </Switch>
